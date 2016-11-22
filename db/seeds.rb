ab28c5c874dbb02abdd66fb2ac7daa73fd0d587a
Etienne Curati <etienne.curati@gmail.com>
2016-11-22 16:20:30 -0200

    Finish exercie 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+require 'faker'
+
+# Creating 100 fake restaurants
+10.times do
+   article = Article.new(
+     title:    Faker::Lorem.sentence,
+     content: Faker::Lorem.paragraphs(5),
+   )
+   article.save!
+end
